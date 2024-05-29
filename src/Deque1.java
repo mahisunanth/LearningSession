73c51acb2660c7f7c729c41575b7fd639be4e493
Mekala Sunanth <mahisunanth1437@gmail.com>
2024-05-29 07:41:03 +0530

    added class

---
+++ src/Deque1.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class Deque1 {
+	public static void main(String args []) {
+		Deque<String> dq= new ArrayDeque <String>();
+		
+		dq.add("gautam");
+		
+		dq.add("ramu");
+		
+		dq.add("abhi");
+		
+		dq.add("ganu");
+		
+		for(String str : dq) {
+			
+			System.out.println(str);
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
